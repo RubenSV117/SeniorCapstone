b1f02701f8ee4ac7ca1bd0cb1999e3af250f1b17
breadmoirai <breadmoirai@gmail.com>
2019-02-19 21:06:46 -0800

    Add Test Scripts

---
+++ Assets/Tests/AuthenticationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AuthenticationTestScript
+    {
+        private static readonly string TEST_EMAIL = "TestEmail@FakeDomain.com";
+        private static readonly string TEST_PASSWORD = "NotARealPassword";
+
+
+        private Authenticator auth;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            auth = new Authenticator();
+            auth.SignOut();
+        }
+
+        [Test]
+        public void CreateAccountEmail()
+        {
+            auth.user
+            auth.RegisterUserWithEmail(TEST_EMAIL, TEST_PASSWORD).ContinueWith(user =>
+            {
+                Assert.NotNull(user);
+            });
+
+            auth.RegisterUserWithEmail(TEST_EMAIL, TEST_PASSWORD).ContinueWith(user =>
+            {
+                Assert.IsNull(user);
+            });
+        }
+
+        [Test]
+        public void TEST()
+        {
+            TaskResult<string, Exception> result = new TaskResult<string, Exception>();
+            result.is
+        }
+
+    }
+}
---
+++ Assets/Tests/LoginServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LoginServiceTest
+    {
+        private static readonly string TEST_EMAIL = "TestEmail@FakeDomain.com";
+        private static readonly string TEST_PASSWORD = "NotARealPassword";
+
+
+        private LoginService auth;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            auth = new LoginService();
+            auth.SignOut();
+        }
+
+        [Test]
+        public void CreateAccountEmail()
+        {
+            var handler = new Mock<(typeof(ILoginServiceEventHandler)); 
+            auth.RegisterUserWithEmail(TEST_EMAIL, TEST_PASSWORD).WithSuccess(user =>
+            {
+                Assert.NotNull(user);
+            });
+
+            auth.RegisterUserWithEmail(TEST_EMAIL, TEST_PASSWORD).ContinueWith(user =>
+            {
+                Assert.IsNull(user);
+            });
+        }
+
+        [Test]
+        public void TEST()
+        {
+            TaskResult<string, Exception> result = new TaskResult<string, Exception>();
+            result.is
+        }
+
+    }
+}

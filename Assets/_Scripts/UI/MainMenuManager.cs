161125b71bdb54993c031d388910a78d2bc86947
Ruben Sanchez <SanchezVRuben@gmail.com>
2019-03-16 13:14:09 -0700

    MainMenuManger

---
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Transform recipeListTrans;
+
+    public void SearchForRecipes(string recipeName)
+    {
+
+    }
+
+    public void RefreshRecipeList(List<Recipe>)
+
+}

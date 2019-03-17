897816e81905ea93ad8aa43e540ba9eee0d2b0e1
Jewishuwereme <43190248+Jewishuwereme@users.noreply.github.com>
2019-03-16 18:50:43 -0700

    Query elasticsearch script + restsharp added to assets/packages

--- Assets/_Scripts/Database/DatabaseManager.cs
+++ Assets/_Scripts/Database/DatabaseManager.cs
@@ -4,11 +4,13 @@ using Firebase.Database;
 using Firebase.Unity.Editor;
 using UnityEngine;
 using Object = System.Object;
+using Restsharp;
+
 
 public class DatabaseManager : MonoBehaviour
 {
     public static DatabaseManager Instance;
-
+    
     private DatabaseReference databaseReference;
 
     private void Awake()
@@ -18,7 +20,6 @@ public class DatabaseManager : MonoBehaviour
 
         // Set up the Editor before calling into the realtime database.
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://regen-66cf8.firebaseio.com/");
-
         // Get the root databaseReference location of the database.
         databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
 
@@ -35,7 +36,22 @@ public class DatabaseManager : MonoBehaviour
 
         databaseReference.Child("recipes").Child(recipe.Name).SetRawJsonValueAsync(json);
     }
-
+    private void testWebrequestName(string name)
+    {
+        var client = new RestClient("http://35.192.138.105/elasticsearch/_search/template");
+        var request = new RestRequest(Method.GET);
+        request.AddHeader("Postman-Token", "e4f474bc-ca7c-4853-a2ec-27f7e5748c88");
+        request.AddHeader("cache-control", "no-cache");
+        request.AddHeader("Authorization", "Basic dXNlcjpYNE1keTVXeGFrbVY=");
+        request.AddHeader("Content-Type", "application/json");
+        request.AddParameter("undefined", "{\"source\": { \"query\": {\"bool\": {\"must_not\": [ {\"wildcard\": " +
+            "{\"{{my_field1}}\": \"*{{my_value}}*\"}},{\"fuzzy\": {\"{{my_field1}}\": \"{{my_value}}\"}}, {\"wildcard\": " +
+            "{\"{{my_field2}}\": \"*{{my_value}}*\"}},{\"fuzzy\": {\"{{my_field2}}\": \"{{my_value}}\"}},{\"wildcard\": {\"{{my_field3}}\": " +
+            "\"*{{my_value}}*\"}},{\"fuzzy\": {\"{{my_field3}}\": \"{{my_value}}\"}}]}},\"size\": \"{{my_size}}\"},\"params\": {\"my_field1\": " +
+            "\"name\",\"my_field2\": \"ingredients\",\"my_field3\": \"tags\",\"my_value\": \"endies\",\"my_size\": 100}}", ParameterType.RequestBody);
+        IRestResponse response = client.Execute(request);
+        Console.WriteLine(response.Content);
+    }
     private void TestRead(string name)
     {
         FirebaseDatabase.DefaultInstance
@@ -54,6 +70,7 @@ public class DatabaseManager : MonoBehaviour
                 }
             });
     }
+
 
     private void TestPublish(string name)
     {

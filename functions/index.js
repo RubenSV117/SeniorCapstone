cf5b0235f299e38f75bc25bee0ebfc69c987e464
Jewishuwereme <43190248+Jewishuwereme@users.noreply.github.com>
2019-03-17 01:19:09 -0700

    Changed the function for firebase should work now

--- functions/index.js
+++ functions/index.js
@@ -11,34 +11,34 @@ const _ = require('lodash');
 const request = require('request-promise');
 
 exports.indexRecipesToElastic = functions.database.ref('regen/recipes/{ID}')
-	.onWrite(event => {
-		let recData = event.data.val();
-		let RID   = event.params.I;
+    .onWrite(event => {
+        let recData = event.data.val();
+        let RID = event.params.ID;
 
-		console.log('Indexing Recipe', RID, recData);
-        
-		let elasticSearchConfig = functions.config().elasticsearch;
-		let elasticSearchUrl = elasticSearchConfig.url + '/regen/recipes/' + RID;
-		let elasticSearchMethod = recData ? 'POST' : 'DELETE';
+        console.log('Indexing Recipe', RID, recData);
 
-		let elasticsearchRequest = {
-			method: elasticSearchMethod,
-			uri: elasticSearchUrl,
-			auth: {
-				username: elasticSearchConfig.username,
-				password: elasticSearchConfig.password,
+        let elasticSearchConfig = functions.config().elasticsearch;
+        let elasticSearchUrl = elasticSearchConfig.url + '/regen/recipes/' + RID;
+        let elasticSearchMethod = recData ? 'POST' : 'DELETE';
+
+        let elasticsearchRequest = {
+            method: elasticSearchMethod,
+            uri: elasticSearchUrl,
+            auth: {
+                username: elasticSearchConfig.username,
+                password: elasticSearchConfig.password,
             },
             body: {
                 name: recData.Name.toLowerCase(),
-                ingredients: Object.keys(recData.Ingredients).map(function (key) { recData.Ingredients[key].IngredientName.toLowerCase()}),
-                tags: Object.keys(recData.Tags).map(function (key) { recData.Tags[key].toLowerCase() }),
+                ingredients: Object.keys(recData.Ingredients).map(function (key) { recData.Ingredients[key].IngredientName.toLowerCase() }),
+                tags: Object.keys(recData.Tags).map(function (key) { recData.Tags[key].toLowerCase()}),
             },
-			json: true
-		};
+            json: true
+        };
 
-		return request(elasticsearchRequest).then(response => {
+        return request(elasticsearchRequest).then(response => {
             console.log('Elasticsearch response', response);
             return true;
-		})
+        })
 
-	});
+    });

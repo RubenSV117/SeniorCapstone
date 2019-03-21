1e366d64005e044cea492605bc2f5572b047b2db
Kyle Wu <kylewu2@gmail.com>
2019-03-21 11:46:53 -0700

    fixed typo in code

--- Assets/_Scripts/UI/SearchManagerUI.cs
+++ Assets/_Scripts/UI/SearchManagerUI.cs
@@ -36,7 +36,7 @@ public class SearchManagerUI : MonoBehaviour
         Debug.Log($"Searching {recipeName} with {TagsToExlude.Count} tags to exclude...");
 
         //DatabaseManager.Instance.Search(recipeName);
-        DatabaseManager.Instance.elasticSearchExclude(recipeName, tagsToExlude.ToArray());
+        DatabaseManager.Instance.elasticSearchExclude(recipeName, TagsToExlude.ToArray());
     }
 
     public void SearchForRecipesSimple(string recipeName)

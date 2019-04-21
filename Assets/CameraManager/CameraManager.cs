f9c52bad1c2077c7d6c0ffcbba29b3782943c103
Ruben Sanchez <SanchezVRuben@gmail.com>
2019-04-20 23:56:48 -0700

    Camera plugins

---
+++ Assets/CameraManager/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+///
+///
+/// Ruben Sanchez
+/// 
+/// </summary>
+public class CameraManager : MonoBehaviour
+{
+    public void TakePicture()
+    {
+        NativeCamera.TakePicture(TakePictureCallback);
+    }
+
+    private void TakePictureCallback(string path)
+    {
+        print($"PATH: {path}");
+    }
+}

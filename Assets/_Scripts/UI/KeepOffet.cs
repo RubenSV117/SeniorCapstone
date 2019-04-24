6c865678a8b1d72ca154136b379e16d6f4831916
Ruben Sanchez <SanchezVRuben@gmail.com>
2019-04-23 22:28:53 -0700

    Screen size scaling for  KeepOffset class

--- Assets/_Scripts/UI/KeepOffet.cs
+++ Assets/_Scripts/UI/KeepOffet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Keeps this rect transform at an offset under another rect transform
@@ -10,6 +11,12 @@ public class KeepOffet : MonoBehaviour
     [SerializeField] private RectTransform rectTransToMove;
     [SerializeField] private RectTransform targetRectTrans;
     [SerializeField] private Vector2 offset;
+    [SerializeField] private CanvasScaler scaler;
+
+    private void Awake()
+    {
+        offset *= (new Vector2(Screen.width, Screen.height)) / scaler.referenceResolution;
+    }
 
     private void Update()
     {
